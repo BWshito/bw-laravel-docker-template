ded13a64a32e3c6e0fd4d5c56d671a0d44208ceb
BWshito <ayas410.hrn@gmail.com>
2025-12-23 00:03:48 +0900

    feat: 論理削除による削除機能の追加

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'todos';
 
     // fill()によって代入可能なプロパティ
